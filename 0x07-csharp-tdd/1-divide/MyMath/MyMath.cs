76b82e1d9d87b3185116bfdb3908fbb1fb9193a9
FQX117 <blackfox950@gmail.com>
2022-09-12 17:25:17 -0500

    new way

--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -6,28 +6,23 @@ namespace MyMath
     {
         public static int[,] Divide(int[,] matrix, int num)
         {
-            try
-            {
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
-                    {
-                        matrix[i, j] = matrix[i, j] / num;
-                    }
-                }
-                return matrix;
-            }
-            catch
-            {
-                if (num == 0)
-                    Console.WriteLine("Num cannot be 0");
-                    return null;
-            }
-            catch
-            {
-                if (matrix == null)
-                    return null;
-            }
-        }
-    }
+			if (num == 0)
+			{
+				Console.WriteLine("Num cannot be 0");
+				return null;
+			}
+			if (matrix == null)
+			{
+				return null;
+			}
+			for (int i = 0; i < matrix.GetLength(0); i++)
+			{
+				for (int j = 0; j < matrix.GetLength(1); j++)
+				{
+					matrix[i, j] = matrix[i, j] / num;
+				}
+			}
+			return matrix;
+		}
+	}
 }
