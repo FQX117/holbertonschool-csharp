c739d446db5662d0987074d761ee2660a323b326
FQX117 <blackfox950@gmail.com>
2022-09-12 18:14:10 -0500

    fix

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 
 namespace MyMath
 {
+    /// <summary>comment</summary>
     public class Operations
     {
+        /// <summary>comment</summary>
         public static int Max(List<int> nums)
             if (nums == null || nums.Count == 0)
             {
