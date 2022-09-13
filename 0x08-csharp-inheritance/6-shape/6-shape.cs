48038dc02b3a52eae3b069125b4835c792ea2707
FQX117 <blackfox950@gmail.com>
2022-09-13 14:08:15 -0500

    add files

---
+++ 0x08-csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,48 @@
+using System
+
+/// <summary>Base Class</summary>
+class Shape
+{
+    /// <summary>error msg</summary>
+    public virtual int Area()
+    {
+        throw new System.NotImplementedException("Area() is not implemented");
+    }
+    
+}
+
+/// <summary>rectangle</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    //width of rectangle
+    public int width
+    {
+        get
+        {
+            return width;
+        }
+        set
+        if (value < 0)
+        {
+            throw new ArgumentException("Width must be greater than or equal to 0");
+        }
+        width = value;
+    }
+    //Height of rectangle
+    public int Height
+    {
+        get
+        {
+            return height;
+        }
+        set
+        if (value < 0)
+        {
+            throw new ArgumentException("Height must be greater than or equal to 0");
+        }
+        height = value;
+    }
+}
