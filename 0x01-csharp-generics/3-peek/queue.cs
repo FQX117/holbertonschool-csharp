fcfca80ebb9be1e4bc3b47e3cdc81138c4b93d72
FQX117 <blackfox950@gmail.com>
2022-09-20 21:19:05 -0500

    update 3

--- 0x01-csharp-generics/3-peek/queue.cs
+++ 0x01-csharp-generics/3-peek/queue.cs
@@ -1,95 +1,82 @@
 using System;
 
-   /// <summary>MAkes new class</summary>
-   /// <typeparam name="T"></typeparam>
-    class Queue<T> 
-    {
-      /// <summary>checks type</summary>
-      /// <returns>typeof</returns>
-      public Type CheckType()
-      {
-         return typeof(T);
-      }
-       
-       /// <summary>sets up node</summary>
-       public class Node
-       {
-        private T value = default(T);
-        private Node next = null;
+///<summary>Class for que opperations</summary>
+class Queue<T>
+{
+	//Setting node for queues
+	public class Node
+	{
+		public T value = default(T);
+		public Node next = null;
 
-        public Node(T input)
-        {
-         value = input;
-        }
-       }
+		public Node(T input)
+		{
+			value = input;
+		}
+	}
 
+	public Node head;
+	public Node tail;
+	public int count;
 
-       public Node head;
-       public Node tail;
-       public int count;
-       /// <summary>
-       /// adds node to end
-       /// </summary>
-       /// <param name="value"></param>
-       public void Enqueue(T value)
-       {
-         Node add@ = new Node();
-         if (head == null)
-         {
-            head = add@;
-         }
-         else
-         {
-            var tail = head;
-            while (tail.next != null)
-            {
-               tail = tail.next;
-            }
-            tail.next = add@;
-         }
-         count++;
-       }
-       /// <summary>
-       /// counts nodes
-       /// </summary>
-       /// <returns>count</returns>
-       public int Count()
-       {
-         return count;
-       }
-       /// <summary>
-       /// removes head
-       /// </summary>
-       /// <param name="value"></param>
-       public void Dequeue(T value)
-       {
-        if (head == null)
-        {
-            Console.write("Queue is empty");
-            return default(T);
-        }
-        else 
-        {
-           T value = head.value;
-           Node.Remove(head);
-           return value;
-        }
-       }
-       /// <summary>
-       /// gives head value
-       /// </summary>
-       /// <param name="value"></param>
-       public void Peek(T value)
-       {
-        if (head == null)
-        {
-            Console.write("Queue is empty");
-            return default(T);
-        }
-        else 
-        {
-           T value = head.value;
-           return value;
-        }
-       }
-    }
+	///<summary>Add new node at the end</summary>
+	public void Enqueue(T value)
+	{
+		Node node = new Node(value);
+		if (head == null)
+		{
+			head = node;
+			tail = node;
+		}
+		else
+		{
+			tail.next = node;
+			tail = node;
+		}
+		count++;
+	}
+
+	///<summary>Add deleting the last node</summary>
+	public T Dequeue()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			T value = head.value;
+			head = head.next;
+			count--;
+			return value;
+		}
+	}
+
+	///<summary>Return the value of first node</summary>
+	public T Peek()
+	{
+		if (head == null)
+		{
+			Console.WriteLine("Queue is empty");
+			return default(T);
+		}
+		else
+		{
+			return head.value;
+		}
+	}
+
+	///<summary>Add new node at the end</summary>
+	public int Count()
+	{
+		return count;
+	}
+
+	///<summary>Check if its a queue type</summary>
+	///<return>returns a Queue type</return>
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
+}
