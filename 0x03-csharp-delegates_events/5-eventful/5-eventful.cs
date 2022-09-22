63d6ad0cfa71e4795a1b788a2e8c146d3b6f1d87
FQX117 <blackfox950@gmail.com>
2022-09-22 16:31:13 -0500

    add""

--- 0x03-csharp-delegates_events/5-eventful/5-eventful.cs
+++ 0x03-csharp-delegates_events/5-eventful/5-eventful.cs
@@ -20,7 +20,7 @@ public class Player
         private string status;
         
 
-        public Player(string name = Player, float maxHp = 100f)
+        public Player(string name = "Player", float maxHp = 100f)
         {
             if (maxHp < 1)
             {
