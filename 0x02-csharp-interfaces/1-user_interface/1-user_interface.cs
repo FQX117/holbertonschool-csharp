43b9ae324c9f46551749b298d2438c6c8b95db54
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-09-21 19:47:09 -0500

    update

--- 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x02-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -8,7 +8,7 @@ abstract class Base
     public string name = "";
     public override string ToString()
     {
-    return String.Format("{0} is a {1}", name, this.GetType().Name);
+    return (this.name + " is a " + this.GetType().ToString());
     }
 }
 
