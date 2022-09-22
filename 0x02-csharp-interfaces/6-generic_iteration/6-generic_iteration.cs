7fab3ea2943a6eba6e480f75640037c65a5dba25
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-09-21 19:10:08 -0500

    Update 6-generic_iteration.cs

--- 0x02-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
+++ 0x02-csharp-interfaces/6-generic_iteration/6-generic_iteration.cs
@@ -1,4 +1,4 @@
-using Internal;
+using System.Collections.Generic;
 using System;
 
 abstract class Base
@@ -7,7 +7,7 @@ abstract class Base
     /// base class
     /// </summary>
     public string name = "";
-    public override ToString()
+    public override string ToString()
     {
     return String.Format("{0} is a {1}", name, this.GetType().Name);
     }
@@ -32,7 +32,7 @@ interface ICollectable
 /// <summary>
 /// door class
 /// </summary>
-class Door : IInteractive, Base
+class Door : Base, IInteractive
 {
     public void Interact()
     {
@@ -53,7 +53,7 @@ class Decoration : Base, IInteractive, IBreakable
     {
         if (durability < 1)
         {
-            throw new Exception("Durability must be greater than 0");
+            Console.WriteLine("Durability must be greater than 0");
         }
         this.name = name;
         this.durability = durability;
@@ -64,15 +64,15 @@ class Decoration : Base, IInteractive, IBreakable
     {
         if (durability < 1)
         {
-            throw new Exception("The {0} has been broken.", this.name);
+            Console.WriteLine("The {0} has been broken.", this.name);
         }
-        if (isQuestItem)
+        else if (isQuestItem)
         {
-            Console.WriteLine("You look at the {0}. There's a key inside.", this.name)
+            Console.WriteLine("You look at the {0}. There's a key inside.", this.name);
         }
         else
         {
-            Console.WriteLine("You look at the {0}. Not much to see here.", this.name)
+            Console.WriteLine("You look at the {0}. Not much to see here.", this.name);
         }
     }
     public void Break()
@@ -80,16 +80,16 @@ class Decoration : Base, IInteractive, IBreakable
         this.durability--;
         if (durability < 0)
         {
-            throw new Exception("The {0} is already broken.", this.name);
+            Console.WriteLine("The {0} is already broken.", this.name);
         }
 
         if (durability > 0)
         {
-            Console.WriteLine("You hit the {0}. It cracks.", this.name)
+            Console.WriteLine("You hit the {0}. It cracks.", this.name);
         }
         if (durability == 0)
         {
-            Console.WriteLine("You smash the {0}. What a mess.", this.name)
+            Console.WriteLine("You smash the {0}. What a mess.", this.name);
         }
     }
 }
